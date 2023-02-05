c1e895320e6a7e98cf8e5bb88dadb77edbe55a26
Tebenj <benjamin2103@hotmail.fr>
2023-02-05 18:32:44 +0100

    Design main page

--- app/src/main/java/com/benjamin/projet_android/MainActivity.java
+++ app/src/main/java/com/benjamin/projet_android/MainActivity.java
@@ -2,7 +2,9 @@ package com.benjamin.projet_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Historique(View view) {
+        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void Parametre(View view){
+        Intent intent = new Intent(MainActivity.this, ParametersActivity.class);
+        startActivity(intent);
+    }
 }
+
