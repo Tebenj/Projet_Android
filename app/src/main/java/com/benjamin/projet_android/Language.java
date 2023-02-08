e959b46be3feadfaee11a11481c8a5bb4fc2b7b1
Federdispi <federdispi@gmail.com>
2023-02-08 12:01:17 +0100

    Flags added

--- app/src/main/java/com/benjamin/projet_android/Language.java
+++ app/src/main/java/com/benjamin/projet_android/Language.java
@@ -3,10 +3,100 @@ package com.benjamin.projet_android;
 public class Language {
     private String id;
     private String name;
+    private String flag;
 
     public Language(String id, String name) {
         this.id = id;
         this.name = name;
+        switch (id) {
+            case "BG":
+                this.flag = "🇧🇬";
+                break;
+            case "CS":
+                this.flag = "🇨🇿";
+                break;
+            case "DA":
+                this.flag = "🇩🇰";
+                break;
+            case "DE":
+                this.flag = "🇩🇪";
+                break;
+            case "EL":
+                this.flag = "🇬🇷";
+                break;
+            case "EN":
+                this.flag = "🇬🇧";
+                break;
+            case "ES":
+                this.flag = "🇪🇸";
+                break;
+            case "ET":
+                this.flag = "🇪🇪";
+                break;
+            case "FI":
+                this.flag = "🇫🇮";
+                break;
+            case "FR":
+                this.flag = "🇫🇷";
+                break;
+            case "HU":
+                this.flag = "🇭🇺";
+                break;
+            case "ID":
+                this.flag = "🇲🇨";
+                break;
+            case "IT":
+                this.flag = "🇮🇹";
+                break;
+            case "JA":
+                this.flag = "🇯🇵";
+                break;
+            case "KO":
+                this.flag = "🇰🇷";
+                break;
+            case "LT":
+                this.flag = "🇱🇹";
+                break;
+            case "LV":
+                this.flag = "🇱🇻";
+                break;
+            case "NB":
+                this.flag = "🇳🇴";
+                break;
+            case "NL":
+                this.flag = "🇳🇱";
+                break;
+            case "PL":
+                this.flag = "🇵🇱";
+                break;
+            case "PT":
+                this.flag = "🇵🇹";
+                break;
+            case "RO":
+                this.flag = "🇷🇴";
+                break;
+            case "RU":
+                this.flag = "🇷🇺";
+                break;
+            case "SK":
+                this.flag = "🇸🇰";
+                break;
+            case "SL":
+                this.flag = "🇸🇮";
+                break;
+            case "SV":
+                this.flag = "🇸🇪";
+                break;
+            case "TR":
+                this.flag = "🇹🇷";
+                break;
+            case "UK":
+                this.flag = "🇺🇦";
+                break;
+            case "ZH":
+                this.flag = "🇨🇳";
+                break;
+        }
     }
 
     public String getId() {
@@ -18,6 +108,6 @@ public class Language {
     }
 
     public String toString() {
-        return name;
+        return flag + " " + name;
     }
 }
